f8f960193a9aad1dcc87e1420a5e00d13f94a3b7
souzet tadros <souzettadros@gmail.com>
2020-11-09 10:50:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaration:
+            string command;
+            // REPl (do-while-loop)
+            do
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+                //    Om commandot är load: läs filen
+            }
+            while (command != "quit");
+        }
+    }
+}
