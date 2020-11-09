c34f2177ecec48c85c9719e9af346ad1f3483d2a
souzet tadros <souzettadros@gmail.com>
2020-11-09 23:16:06 +0100

    2

--- Program.cs
+++ Program.cs
@@ -11,14 +11,25 @@ namespace ToDoList
     {
         static void Main(string[] args)
         {
-            // Declaration:
+            // 1Declaration:
             string command;
-            // REPl (do-while-loop)
+            // 3Greetings:
+            Console.WriteLine("Hello and welcome todo list");
+            Console.WriteLine("To quit type 'quit', for help type 'help'!");
+            // 2REPL (do-while-loop)
             do
             {
                 Console.Write("> ");
                 command = Console.ReadLine();
                 //    Om commandot är load: läs filen
+                if( command== "quit")
+                {
+                    Console.WriteLine("Bye!");
+                }
+                else
+                {
+                    Console.WriteLine("unknown command: {0}", command);
+                }
             }
             while (command != "quit");
         }
